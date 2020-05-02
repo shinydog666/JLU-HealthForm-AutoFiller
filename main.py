97c25eba8dd2c70beb430a18ed29ae7112bd9f79
ForeverOpp <life.app.hanfeng@hotmail.com>
2020-05-02 10:13:42 +0800

    Add something

--- main.py
+++ main.py
@@ -3,3 +3,11 @@
 https://ehall.jlu.edu.cn/infoplus/form/JLDX_BK_XNYQSB/start
 
 """
+import configparser
+from tkinter import messagebox
+# import win32api,win32con
+messagebox.showinfo("Test","sss")
+# win32api.MessageBox(0, "这是一个测试提醒OK消息框", "提醒",win32con.MB_OK)
+cfg = configparser.ConfigParser()
+cfg.read("config.ini", encoding="utf-8")
+
