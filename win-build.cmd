bfee8ecc961c214772efd3bd7ca7ca27242292a9
ForeverOpp <life.app.hanfeng@hotmail.com>
2020-05-03 13:23:16 +0800

    修复了一点小毛病

--- win-build.cmd
+++ win-build.cmd
@@ -3,7 +3,7 @@ cd %~dp0
 rd /s /q build
 rd /s /q dist
 del *.spec
-pyinstaller -i ./fav.ico -F main.py --onefile -p MessageSender.py
+pyinstaller -i ./fav.ico -w -F main.py --onefile -p MessageSender.py
 xcopy /y dist\*.exe .
 rd /s /q build
 rd /s /q dist
